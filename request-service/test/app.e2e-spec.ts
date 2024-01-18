b3fb093bfcf394c235751b849bdb0e772e67d641
Hamid Sekandari <sekandari.hs@gmail.com>
2024-01-18 11:00:39 +0330

    finalize

--- request-service/test/app.e2e-spec.ts
+++ request-service/test/app.e2e-spec.ts
@@ -2,23 +2,29 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { LibraryModule } from '../src/library.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [LibraryModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/library/request (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/request')
+      .send({
+        requester_name: 'test',
+        requested_book: 'test',
+      })
+      .expect(200);
   });
 });
